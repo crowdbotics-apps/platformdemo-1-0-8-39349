d4000ad9ec34eb0133e7d1419de9d12b353a5189
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-03-22 02:53:32 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "platformdemo-1-0-8-39349",
-  url: "https://platformdemo-1-0-8-39349.botics.co",
-  api: "https://platformdemo-1-0-8-39349.botics.co/api/v1"
+  "name": "platformdemo-1-0-8-39349",
+  "url": "https://platformdemo-1-0-8-39349.botics.co",
+  "api": "https://platformdemo-1-0-8-39349.botics.co/api/v1",
+  "initialRoute": "onboarding-2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
-    copy: "Routes available!"
+    "copy": "Routes available!"
   }
 };
--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,20 +1,37 @@
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
+import { Text } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled1 = () => {
+  const navigation = useNavigation();
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><Pressable><Pressable onPress={() => {
+          navigation.navigate("onboardingStack");
+        }}><Text style={styles.IcyTHUqz}>This is X screen, click me to go to Y screen</Text></Pressable></Pressable></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  IcyTHUqz: {
+    width: 243,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    textAlign: "center",
+    position: "absolute",
+    top: 261.5,
+    left: 57
   }
 });
 export default Untitled1;
